b89f335f75c5bc55b92526b4c8906058753d465a
NENightElves <NENightElves@qq.com>
2022-01-09 09:38:58 +0000

    update main.py

--- micropython/main.py
+++ micropython/main.py
@@ -1,17 +1,19 @@
 import WifiConnector
 import server
-
-flag = True
-
-try:
-    import loop
-except Exception as e:
-    print(e)
-    flag = False
+import sys
+import utime
 
 WifiConnector.connect("wifi_config.json")
 
 server.start()
+
+
+flag = True
+try:
+    import loop
+except Exception as e:
+    sys.print_exception(e)
+    flag = False
 
 if flag:
     print('initialization completed! do loop...')
@@ -19,4 +21,5 @@ if flag:
         try:
             loop.loop()
         except Exception as e:
-            print(e)
+            sys.print_exception(e)
+        utime.sleep(1)
